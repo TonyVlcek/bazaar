2c266b26b191d8e0c54e0786d052163ce609fcb2
TonyVlcek <vlcek.a@email.cz>
2019-05-10 19:41:42 +0200

    Add phinx and create items migration

---
+++ db/migrations/20190510165959_items.php
@@ -0,0 +1,20 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Items extends AbstractMigration
+{
+
+    /**
+     * @throws RuntimeException
+     */
+    public function change()
+    {
+        $table = $this->table('items');
+        $table
+            ->addColumn('detail_url', 'string', ['length' => 2083])
+            ->addColumn('title', 'string', ['length' => 255])
+            ->create();
+    }
+
+}
